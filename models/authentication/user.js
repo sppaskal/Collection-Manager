d9d14e6857d92aa1e09fac44f6db297e47207aed
sppaskal <paskalevs@yahoo.ca>
2024-11-25 11:25:44 -0800

    Adding User model
    - The User model will be used for authentication and to segregate card collections from user to user

---
+++ models/authentication/user.js
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose'
+import bcrypt from 'bcrypt'
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true }
+})
+
+// Pre-save hook to hash the password before saving it
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next()
+
+  try {
+    const salt = await bcrypt.genSalt(10)
+    this.password = await bcrypt.hash(this.password, salt)
+    next()
+  } catch (err) {
+    next(err)
+  }
+})
+
+// Method to compare password for authentication
+userSchema.methods.comparePassword = async function (password) {
+  return bcrypt.compare(password, this.password)
+}
+
+export default mongoose.model('User', userSchema)
