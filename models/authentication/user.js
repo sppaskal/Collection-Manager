cfc2bc9a8de8327df3ea519ea2e3cbe6c941b99c
sppaskal <paskalevs@yahoo.ca>
2024-11-25 11:57:01 -0800

    Updating user model naming and capitalization
    - This was done to conform to previously used standard in other models

--- models/authentication/user.js
+++ models/authentication/user.js
@@ -1,13 +1,13 @@
 import mongoose from 'mongoose'
 import bcrypt from 'bcrypt'
 
-const userSchema = new mongoose.Schema({
+const User = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true }
 })
 
 // Pre-save hook to hash the password before saving it
-userSchema.pre('save', async function (next) {
+User.pre('save', async function (next) {
   if (!this.isModified('password')) return next()
 
   try {
@@ -20,8 +20,8 @@ userSchema.pre('save', async function (next) {
 })
 
 // Method to compare password for authentication
-userSchema.methods.comparePassword = async function (password) {
+User.methods.comparePassword = async function (password) {
   return bcrypt.compare(password, this.password)
 }
 
-export default mongoose.model('User', userSchema)
+export default mongoose.model('user', User)
